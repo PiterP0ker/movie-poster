cf67e37c5f851d420954eb078cfebf21f89f910f
PiterP0ker <piterp0ker2294@gmail.com>
2020-05-26 16:52:18 +0300

    Updates

---
+++ precache-manifest.caf5e4f2924f8baa058d60b5c6f38a50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b813e4a41cb39b9d40baf1fd11e85b71",
+    "url": "//piterp0ker.github.io/movie-poster/index.html"
+  },
+  {
+    "revision": "98602eab1abad159eec0",
+    "url": "//piterp0ker.github.io/movie-poster/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a5f4e10710c70341ac4a",
+    "url": "//piterp0ker.github.io/movie-poster/static/css/main.cab8b1b3.chunk.css"
+  },
+  {
+    "revision": "98602eab1abad159eec0",
+    "url": "//piterp0ker.github.io/movie-poster/static/js/2.339b993c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//piterp0ker.github.io/movie-poster/static/js/2.339b993c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f4e10710c70341ac4a",
+    "url": "//piterp0ker.github.io/movie-poster/static/js/main.36cbc534.chunk.js"
+  },
+  {
+    "revision": "f2e4f0d6076e9022362f",
+    "url": "//piterp0ker.github.io/movie-poster/static/js/runtime-main.f73d45be.js"
+  }
+]);
